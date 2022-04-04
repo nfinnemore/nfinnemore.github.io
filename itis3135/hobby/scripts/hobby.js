ef49a09019c916562aa42582a6fe8280e0f89d29
nfinnemore <nickf4950@gmail.com>
2022-04-03 22:09:40 -0400

    added hobby

---
+++ itis3135/hobby/scripts/hobby.js
@@ -0,0 +1,48 @@
+function switchSection1() {
+    section1.style.display = "block";
+    section2.style.display = "none";
+    section3.style.display = "none";
+    section4.style.display = "none";
+    section5.style.display = "none";
+    section6.style.display = "none";
+}
+function switchSection2() {
+    section1.style.display = "none";
+    section2.style.display = "block";
+    section3.style.display = "none";
+    section4.style.display = "none";
+    section5.style.display = "none";
+    section6.style.display = "none";
+}
+function switchSection3() {
+    section1.style.display = "none";
+    section2.style.display = "none";
+    section3.style.display = "block";
+    section4.style.display = "none";
+    section5.style.display = "none";
+    section6.style.display = "none";
+}
+function switchSection4() {
+    section1.style.display = "none";
+    section2.style.display = "none";
+    section3.style.display = "none";
+    section4.style.display = "block";
+    section5.style.display = "none";
+    section6.style.display = "none";
+}
+function switchSection5() {
+    section1.style.display = "none";
+    section2.style.display = "none";
+    section3.style.display = "none";
+    section4.style.display = "none";
+    section5.style.display = "block";
+    section6.style.display = "none";
+}
+function switchSection6() {
+    section1.style.display = "none";
+    section2.style.display = "none";
+    section3.style.display = "none";
+    section4.style.display = "none";
+    section5.style.display = "none";
+    section6.style.display = "block";
+}
