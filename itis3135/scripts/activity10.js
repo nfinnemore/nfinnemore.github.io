cf65616fadd82408df8ca88cb919c7cf8a70b182
nfinnemore <nickf4950@gmail.com>
2022-04-04 23:05:26 -0400

    fixed css and image

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -6,18 +6,16 @@ $(document).ready(function()
         image.src = $(this).attr("href");
     });
     // set up the event handlers for each link
-            $("#image_list a").click(function(e) {
-                let imagePath = $(this).attr("href");
-                let caption = $(this).attr("title");
-                $("#caption").text(caption);
-                $("#image").hide().fadeIn("1000").attr("src",imagePath);
-            
-
+    $("#image_list a").click(function(event) {
+        let imagePath = $(this).attr("href");
+        let caption = $(this).attr("title");
+        $("#caption").text(caption);
+        $("#image").hide().fadeIn("1000").attr("src",imagePath)
         // cancel the default action of each link
-                e.preventDefault();
-            });
+        event.preventDefault();
+        });
 
 // move the focus to the first link
-$('a').first().focus();
+$("li:first-child a").focus();
 
 }); // end ready
