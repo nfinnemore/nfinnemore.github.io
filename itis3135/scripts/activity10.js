34d11afafc07d0575de6c0a3e5b549487964b7a8
nfinnemore <nickf4950@gmail.com>
2022-04-04 21:27:58 -0400

    added activity 10

---
+++ itis3135/scripts/activity10.js
@@ -0,0 +1,26 @@
+$(document).ready(function() 
+{
+
+
+    // preload the image for each link
+    $("#image_list a").each(function() {
+        let caption = $(this).attr("title");
+        let image = new Image();
+        image.src = $(this).attr("href");
+    });
+    // set up the event handlers for each link
+            $("#image_list a").click(function(e) {
+                let imagePath = $(this).attr("href");
+                let caption = $(this).attr("title");
+                $("#caption").text(caption);
+                $("#image").attr("src",imagePath);
+            
+
+        // cancel the default action of each link
+                e.preventDefault();
+            });
+
+// move the focus to the first link
+$("li:first-child a").focus();
+
+}); // end ready
