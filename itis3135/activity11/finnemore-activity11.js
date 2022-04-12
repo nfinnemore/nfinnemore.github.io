e68727683213f4d655edd6310cc332fb8725b1cd
nfinnemore <72285328+nfinnemore@users.noreply.github.com>
2022-04-12 00:08:10 -0400

    Add files via upload

---
+++ itis3135/activity11/finnemore-activity11.js
@@ -0,0 +1,8 @@
+// jQuery call to the accordion() method. -->
+$(document).ready(function() {
+    $("#accordion").accordion({
+        event: "mouseover",
+        heightStyle: "content",
+        collapsible: true
+    });
+});
