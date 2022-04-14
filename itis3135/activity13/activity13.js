d3f3c588cbebf8a9de504a50011e6b916884f865
nfinnemore <nickf4950@gmail.com>
2022-04-14 19:07:01 -0400

    Update activity13.js

--- itis3135/activity13/activity13.js
+++ itis3135/activity13/activity13.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
-    $.ajax({
-        type: "get",
-        url: "team.json",
-        beforeSend: function() {
-            $("#team").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(xhr, status, error) {
-            alert("Error: " + xhr.status + " - " + error);
-        },
-        dataType: "xml",
-        success: function(data) {
-            $("#team").html("");
-            $(data).find("management").children().each(function() {
-                var xmlDoc = $(this);
-                $("#team").append
-                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
-                          xmlDoc.find("title").text() + "<br>" +
-                          xmlDoc.find("bio").text() + "<br>");
+    $.getJSON("team.json", function(data) {
+        $.each(data, function() {
+            $.each(this, function(key, value) {
+                $("#team").append(
+                    "Name: " + value.name + "<br>" +
+                    "Title: " + value.title + "<br>" +
+                    "Bio: " + value.bio + "<br><br>"
+                );
             });
-        }
+        });
     });
 });
+  
