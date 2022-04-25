b59bb332fda436e04ed907d2f13f589fb65fbccb
nfinnemore <nickf4950@gmail.com>
2022-04-24 20:49:16 -0400

    activity 15

---
+++ itis3135/activity15/activity15.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "get",
+        url: "facultyList.json",
+        beforeSend: function() {
+            $("main").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("main").html("");
+            $.each(data,function(){
+                $.each(facultyMembers, function(key,value){
+                    $("main").append(
+                        "<img src = " + value.image + ">" +
+                        "<h2>" + value.full_name + "</h1>" + 
+                        "<h3>" + value.department + "</h2>" + 
+                        "<p>" + value.bio + "</p>" 
+                    )
+                })
+            })
+        }
+    });
+});
